83506237355618f039f1983f32988e7345c78a3a
JessCP25 <jpayanoc25@gmail.com>
2023-08-28 18:40:17 -0500

    prueba

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const $links = document.getElementById('links');
+const $name = document.querySelector('h1');
+const $socialMedia = document.getElementById('social-media');
+const $description = document.getElementById('description');
+
+const data = {
+    name: "Jessica Payano",
+    nickname: "@JessicaPC25",
+    description: "Frontend Developer | #JavaScript | #React ",
+    avatar: "https://lh3.googleusercontent.com/a/AAcHTtc3qE4yDqsu8U9glF9U6L1HSGloy8e-8yYADZCJhHgNFQ=s288-c-no",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/jpayanoc25",
+        username: "jpayanoc25",
+      },
+      {
+        name: "instagram",
+        url: "https://instagram.com/",
+        username: "",
+      },
+    ],
+    links: [
+      {
+        name: "blog",
+        url: "https://example.com/",
+        color: "red",
+        emoji: "📖",
+      },
+      {
+        name: "podcast",
+        url: "https://example.com/",
+        color: "yellow",
+        emoji: "💬",
+      },
+    ],
+    footer: "Made with Love on Colombia",
+};
+
+const main = () => {
+    let name = document.createTextNode(data?.name);
+    let links = data?.links?.map((link)=>{
+        return `
+        <div class="bg-${link.color}-200 px-4 py-5 w-full flex justify-between">
+          <a class="text-sm font-bold text-${link.color}-600 text-center hover:text-${link.color}-800 cursor-pointer"
+            href="${link.url}" target="_blank">
+            ${link.name}
+          </a>
+          <span>${link.emoji}</span>
+        </div>
+        `
+    }).join('');
+    let newItem = document.createElement('section');
+    newItem.innerHTML = links;
+    $links.appendChild(newItem);
+    $name.appendChild(name);
+}
+
+const $lastName = document.querySelector('h2');
+$lastName.textContent = data?.nickname
+
+$description.textContent = data?.description;
+
+const $imgAvatar = document.querySelector('#avatar');
+$imgAvatar.src = data?.avatar;
+
+console.log('Hola');
+main();
